7c00794da61ef4b0008888e02e10afd9aa207717
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2024-10-11 16:18:04 +0530

    Update dependencies and add React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+
+// Video Timestamp - 1:01:53
