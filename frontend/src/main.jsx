9b8ad90ecefc7d35b79e5983321bcb92434a919f
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2024-10-13 19:26:49 +0530

    Refactor frontend configuration and add proxy for backend API

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,7 +12,4 @@ createRoot(document.getElementById('root')).render(
       </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
-)
-
-
-// Video Timestamp - 1:01:53
+);
