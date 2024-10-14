b9e2c11234a8801a151dc3a66b815337fed05ae3
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2024-10-14 22:50:09 +0530

    Refactor server.js and package.json for frontend configuration and deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 // import Product from "./models/product.model.js";
+import path from "path";
 
 dotenv.config();
 
@@ -11,9 +12,19 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(4000, () => {
     connectDB();
